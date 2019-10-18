b0f7119b9510952dda0664074755cba79889da79
MegaColorBoy <abdushshakoor1992@hotmail.com>
2019-10-18 17:23:20 +0400

    first commit

---
+++ connect.php
@@ -0,0 +1,23 @@
+<?php
+$dbname = 'test';
+$dbuser = 'user';
+$dbpass = 'password';
+$dbhost = '192.168.33.10';
+
+$link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
+
+$test_query = "SHOW TABLES FROM $dbname";
+$result = mysqli_query($link, $test_query);
+$tblCnt = 0;
+
+while($tbl = mysqli_fetch_array($result)) {
+  $tblCnt++;
+}
+
+if (!$tblCnt) {
+  echo "There are no tables<br />\n";
+} else {
+  echo "There are $tblCnt tables<br />\n";
+}
+?>
